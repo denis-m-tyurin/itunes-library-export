a59bfcd5a0642f8b94641e48232e2b226e7aad5e
denis-m-tyurin <denis.m.turin@gmail.com>
2012-12-12 20:23:46 +0400

    Add initial translit support
    Signed-off-by: denis-m-tyurin <denis.m.turin@gmail.com>

--- export_library.pl
+++ export_library.pl
@@ -2,30 +2,36 @@
 use warnings;
 use strict;
 
-#use utf8;
-
 use Mac::iTunes::Library;
 use Mac::iTunes::Library::XML;
 use Mac::iTunes::Library::Item;
 use URI::Escape;
 use Encode qw(encode decode is_utf8);
+use Lingua::Translit;
 
-my %ignore_list = ("Юмор" => "", "Новый плейлист 1" => "", "" => "");
+my $tr = new Lingua::Translit("ALA-LC RUS");
 
-my $usage = "Usage: parse_itunes.pl <library.xml> <destination_dir>\n";
+my %ignore_list = ("Музыка" => "", "Новый плейлист 1" => "");
 
-die $usage if (scalar(@ARGV) != 2);
+my $usage = "Usage: export_library.pl <library.xml> <destination_dir> [--translit]\n";
+
+die $usage if (scalar(@ARGV) < 2);
 my $file = $ARGV[0];
 my $dest_dir = $ARGV[1];
 
-# Make a new Library
+my $translit = 0;
+
+if ($ARGV[2])
+{
+	$translit = 1 if ($ARGV[2] eq "--translit");
+}
+
 print "Loading '$file'...";
 my $library = Mac::iTunes::Library::XML->parse($file);
 print " loaded " . $library->num() . " items.\n"; 
 
 
 # Get the hash of playlists
-
 my $playlist = Mac::iTunes::Library::Playlist->new();
 
 my %playlists = $library->playlists();
@@ -39,20 +45,30 @@ while(($playlist_key,$playlist_value) = each %playlists){
 	if  ($playlist_value->{'items'} )  
 	{
 
-
 	print "Processing playlist: ";
-	print encode("cp866", $playlist_value->name());
-	print "  ";
-	print "\n";
 
+	# encode any strings being printed into cp866 as Windows's command line emulator uses this encoding
+	print encode("cp866", $playlist_value->name());
+	print "  \n";
+	
 	my $playlistname = encode("cp1251", $playlist_value->name());
+
+	# ignore playlists from the ignore list (see definition of the list above)
 	if ( exists($ignore_list{"$playlistname"}) ) {
     	print "Ignore this playlist\n\n";
+    	next;
 	}
 
 	# Create directory for the playlist
-
-	mkdir(encode("cp1251", $playlist_value->name()));
+	if ($translit)
+	{
+		mkdir(encode("cp1251", $tr->translit($playlist_value->name())));
+	}
+	else
+	{
+		mkdir(encode("cp1251", $playlist_value->name()));
+	}
+	
 	
 	my @items = $playlist_value->items();
 
